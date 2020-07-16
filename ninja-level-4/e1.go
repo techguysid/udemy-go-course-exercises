a5ba2b8f40a9b5cfce89c4c77a95203d8e3bd2bb
Siddharth Udeniya <techguysid.23@gmail.com>
2020-07-16 23:27:24 +0530

    Appending to and deleting from slice

--- ninja-level-4/e1.go
+++ ninja-level-4/e1.go
@@ -5,7 +5,7 @@ import (
 )
 
 //In Go for arrays you mention the size while declaring it but not for slice
-func main() {
+func e1() {
 	var x = [5]int{1, 2, 4, 6, 8}
 	fmt.Println(x)
 	for _, v := range x {
--- ninja-level-4/e5.go
+++ ninja-level-4/e5.go
@@ -5,14 +5,8 @@ import (
 )
 
 //In Go for slices you do not mention the size while declaring it but not for slice
-func e4() {
+func main() {
 	var x = []int{1, 2, 4, 6, 8, 10, 12, 14, 16, 18} //Not mentioned the size here
-	x = append(x, 53)
+	x = append(x[:4], x[6:]...)                      //Deleting from slice..using append and slicing the slice
 	fmt.Println(x)
-	x = append(x, 54, 55, 56)
-	fmt.Println(x)
-	y := []int{57, 58, 59, 60}
-	x = append(x, y...)
-	fmt.Println(x)
-
 }
